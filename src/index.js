547d8907ae4e8edc34a368e05d159fd178ffc999
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-06-08 23:41:40 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './scss/normalize.css';
-import './scss/styles.css';
-import 'webpack-material-design-icons';
-import 'basiclightbox';
-// import * as basicLightbox from 'basiclightbox'
--- src/js/modal.js
+++
@@ -1,7 +0,0 @@
-import * as basicLightbox from 'basiclightbox';
-
-function createModal(imagePath) {
-    basicLightbox.create(`<img src="${imagePath}" width="1280">`,).show();
-}
-
-export default createModal;
