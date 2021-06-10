76758cf0cb7cd964c9fd97d000c7a12eb899596c
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-06-10 17:30:01 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,28 +1,59 @@
-// https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
-import error from './pnotify'
+import {
+    alertNoMoreImages,
+    alertLastImages,
+    alertSeachPerformed,
+    alertUnforseenError,
+    alertBadQuery,
+} from './pnotify';
+
+const KEY = "21828776-3a3234db6b008ce4834511463";
+const URL = 'https://pixabay.com/api/';
+const TUNING = 'image_type=photo&orientation=horizontal';
+
 export default class ApiService {
     constructor() {
-        this.page = 1;
+        this.currentPage = 1;
         this.searchQuery = '';
-
+        this.selectData = 'all';
 
     };
 
-    async fetchContent() {
-        const KEY = "21828776-3a3234db6b008ce4834511463";
-        const URL = 'https://pixabay.com/api/';
-        const TUNING = 'image_type=photo&orientation=horizontal';
-        try {
-            const queryFetch = await fetch(`${URL}?${TUNING}&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`)
-            const result = await queryFetch.json();
-            this.increment();
-            return result;
-        } catch {
-            return error();
-        }
-
-
+   
+    fetchContent() {          
+         const url = `${URL}?${TUNING}&q=${this.searchQuery}&image_type=${this.selectData}&page=${this.currentPage}&per_page=12&key=${KEY}`
+        return fetch(url).then(res => res.json()).then(({hits}) => {
+            this.currentPage += 1;            
+            return hits;
+        });
     }
+
+    // async fetchContent() {       
+    //     try {
+    //         const queryFetch = await fetch(`${URL}?${TUNING}&q=${this.searchQuery}&page=${this.currentPage}&per_page=12&key=${KEY}`)
+    //         const result = await queryFetch.json();
+    //         return result;
+    //     } catch {
+    //         return alertBadQuery();
+    //     }
+    // }
+
+    incrementPage() {
+        this.currentPage += 1;
+    }
+
+    resetPage() {
+        this.currentPage = 1
+    }
+
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
 
     increment() {
         this.page += 1;
