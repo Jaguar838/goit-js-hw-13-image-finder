c2f1a44ceac65b9ed34e4566c1d2864345c50c25
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-06-11 07:21:10 +0300

    Update renderPage.js

--- src/js/renderPage.js
+++ src/js/renderPage.js
@@ -8,13 +8,13 @@ import {
     alertSeachPerformed,
     alertUnforseenError,
     alertBadQuery,
-  } from './pnotify';
+} from './pnotify';
 
 const imgCardRef = document.querySelector('.gallery');
 const searchFormRef = document.querySelector('#search-form');
-const headerFormRef = document.querySelector('.search-form__header');
 const loadMoreBtnRef = document.querySelector('[data-action="load-more"]');
 const bottomElement = document.querySelector('.bottom-element');
+
 const API = new ApiService();
 
 searchFormRef.addEventListener('submit', hendlerInput);
@@ -22,67 +22,58 @@ loadMoreBtnRef.addEventListener('click', getFetchData);
 imgCardRef.addEventListener('click', handleOpenModal);
 
 
-function hendlerInput(e){    
-        e.preventDefault();                
-        API.resetPage();
-        API.query = e.currentTarget.query.value.trim();  
-        API.selectData = e.currentTarget.select.value;
-        clearInput();  
-        API.searchQuery ? getFetchData() : alertBadQuery();
+function hendlerInput(e) {
+    e.preventDefault();
+    API.resetPage();
+    API.query = e.currentTarget.query.value.trim();
+    API.selectData = e.currentTarget.select.value;
+    clearInput();
+    API.searchQuery ? getFetchData() : alertBadQuery();
 }
 
-function getFetchData(){ 
+function getFetchData() {
     loadMoreBtn.disable();
     API.fetchContent().then(img => {
-        renderingImgCard(img); 
+        renderingImgCard(img);
     })
-    .catch(error404)    
+        catch (alertSeachPerformed())
 }
 
-function renderingImgCard(hits){ 
-    if(hits.length !== 0) {
+function renderingImgCard(hits) {
+    if (hits.length !== 0) {
         imgCardRef.insertAdjacentHTML('beforeend', cardImgTMPL(hits));
-        loadMoreBtn.show();                
+        loadMoreBtn.show();
         loadMoreBtn.enable();
 
-        if(hits.length < 12) {
+        if (hits.length < 12) {
             headerFormRef.innerHTML = 'There are no more images in this category. Choose another search term';
-            loadMoreBtn.hide(); 
+            loadMoreBtn.hide();
         }
 
-        if(API.currentPage > 2) {
+        if (API.currentPage > 2) {
             toBottom();
-        }                 
-    } else{               
+        }
+    } else {
         alertNoMoreImages()
     }
 }
 
-function error404(){ 
-    clearInput();     
-    loadMoreBtn.hide();  
-    headerFormRef.innerHTML = 'Specify the request more correctly';
-    headerFormRef.classList.add('red');
-}
-
-function clearInput() {  
-    searchFormRef.query.value = '';   
-    headerFormRef.innerHTML = ''; 
-    imgCardRef.innerHTML = '';     
+function clearInput() {
+    searchFormRef.query.value = '';
+    imgCardRef.innerHTML = '';
 }
 
 function handleOpenModal(event) {
     if (event.target.nodeName !== 'IMG') {
-      return;
-    }    
-    const largeImageURL = event.target.dataset.source;  
+        return;
+    }
+    const largeImageURL = event.target.dataset.source;
     createModal(largeImageURL);
-  }
+}
 
-  function toBottom() {
-    console.log(bottomElement);
+function toBottom() {
     bottomElement.scrollIntoView({
-      behavior: 'smooth',
-      block: 'center',
+        behavior: 'smooth',
+        block: 'center',
     });
-  }
+}
