f292818041695f4072e41c93562c346ffa9c8fe7
Jaguar838 <oleg.yamgurov@gmail.com>
2021-06-13 08:43:36 +0300

    add function registerLoadMoreObserver in io.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,6 +1,9 @@
+
+
 const KEY = "21828776-3a3234db6b008ce4834511463";
 const URL = 'https://pixabay.com/api/';
 const TUNING = 'image_type=photo&orientation=horizontal';
+
 export default class ApiService {
     constructor() {
         this.currentPage = 1;
@@ -9,6 +12,7 @@ export default class ApiService {
 
     };
 
+
     fetchContent() {
         const url = `${URL}?${TUNING}&q=${this.searchQuery}&image_type=${this.selectData}&page=${this.currentPage}&per_page=12&key=${KEY}`
         return fetch(url).then(res => res.json()).then(({ hits }) => {
@@ -16,6 +20,16 @@ export default class ApiService {
             return hits;
         });
     }
+
+    // async fetchContent() {       
+    //     try {
+    //         const queryFetch = await fetch(`${URL}?${TUNING}&q=${this.searchQuery}&page=${this.currentPage}&per_page=12&key=${KEY}`)
+    //         const result = await queryFetch.json();
+    //         return result;
+    //     } catch {
+    //         return alertBadQuery();
+    //     }
+    // }
 
     incrementPage() {
         this.currentPage += 1;
