67b20b43634c611b4dd0a07b878f63a9e1cc2918
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-09 22:49:07 +0300

    add code

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,50 +1,45 @@
-
-
-const KEY = "21828776-3a3234db6b008ce4834511463";
+const KEY = '21828776-3a3234db6b008ce4834511463';
 const URL = 'https://pixabay.com/api/';
 const TUNING = 'image_type=photo&orientation=horizontal';
 
 export default class ApiService {
-    constructor() {
-        this.currentPage = 1;
-        this.searchQuery = '';
-        this.selectData = 'all';
+  constructor() {
+    this.currentPage = 1;
+    this.searchQuery = '';
+    this.selectData = 'all';
+  }
 
-    };
+  async fetchContent() {
+    const url = `${URL}?${TUNING}&q=${this.searchQuery}&image_type=${this.selectData}&page=${this.currentPage}&per_page=12&key=${KEY}`;
+    const res = await fetch(url);
+    const { hits } = await res.json();
+    this.incrementPage();
+    return hits;
+  }
 
+  // async fetchContent() {
+  //     try {
+  //         const queryFetch = await fetch(`${URL}?${TUNING}&q=${this.searchQuery}&page=${this.currentPage}&per_page=12&key=${KEY}`)
+  //         const result = await queryFetch.json();
+  //         return result;
+  //     } catch {
+  //         return alertBadQuery();
+  //     }
+  // }
 
-    fetchContent() {
-        const url = `${URL}?${TUNING}&q=${this.searchQuery}&image_type=${this.selectData}&page=${this.currentPage}&per_page=12&key=${KEY}`
-        return fetch(url).then(res => res.json()).then(({ hits }) => {
-            this.incrementPage();
-            return hits;
-        });
-    }
+  incrementPage() {
+    this.currentPage += 1;
+  }
 
-    // async fetchContent() {       
-    //     try {
-    //         const queryFetch = await fetch(`${URL}?${TUNING}&q=${this.searchQuery}&page=${this.currentPage}&per_page=12&key=${KEY}`)
-    //         const result = await queryFetch.json();
-    //         return result;
-    //     } catch {
-    //         return alertBadQuery();
-    //     }
-    // }
+  resetPage() {
+    this.currentPage = 1;
+  }
 
-    incrementPage() {
-        this.currentPage += 1;
-    }
+  get query() {
+    return this.searchQuery;
+  }
 
-    resetPage() {
-        this.currentPage = 1
-    }
-
-
-    get query() {
-        return this.searchQuery;
-    }
-
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
